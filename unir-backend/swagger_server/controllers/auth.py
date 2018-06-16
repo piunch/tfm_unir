05afc1518cecd3838428861d249c7282b8b5db5b
jmtb <javier.munnozt@gmail.com>
2018-06-16 21:02:23 +0200

    Pruebas jwt

--- unir-backend/swagger_server/controllers/auth.py
+++ unir-backend/swagger_server/controllers/auth.py
@@ -1,10 +1,12 @@
 import connexion
 import six
-
+import jwt
+import configparser
 from swagger_server import util
+from json import JSONEncoder
 
 
-def login(login, _pass):  # noqa: E501
+def login(login, password):
     """login
 
     Obtiene un token a partir de usuario y contraseña válidos # noqa: E501
@@ -16,7 +18,17 @@ def login(login, _pass):  # noqa: E501
 
     :rtype: str
     """
-    return 'do some magic!'
+
+    # TODO: previamente hay que verificar los datos de login en la BD
+    # return '', 401
+
+    # obtenemos la clave, creamos el token con los datos del usuario y lo devolvemos en forma de string
+    secret_key = get_token_secret()
+    new_token = jwt.encode({'user': 'miaumiaumiau'},
+                           secret_key, algorithm='HS256')
+    new_token_str = str(new_token)
+
+    return new_token_str, 200
 
 
 def logout():  # noqa: E501
@@ -27,4 +39,22 @@ def logout():  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+
+    token = connexion.request.headers['api_key']
+
+    secret_key = get_token_secret()
+    decoded_token = jwt.decode(token, secret_key, algorithms=['HS256'])
+
+    return str(decoded_token)
+
+
+# Other functions
+
+
+def get_token_secret():
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    secret_key = config['AUTH']['TOKEN_KEY']
+
+    return secret_key
