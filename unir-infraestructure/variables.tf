d9c11ebdf0118d7b1feacac619bd8cb7fa2ec0db
piunch <piunch92@gmail.com>
2018-06-21 15:19:25 +0200

    Primera prueba Terraform + Jenkins

--- unir-infraestructure/variables.tf
+++ unir-infraestructure/variables.tf
@@ -3,3 +3,6 @@ variable "ssh_frontend_key" {}
 variable "ssh_db_key" {}
 variable "base_ami_server" {}
 variable "base_ami_database" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
