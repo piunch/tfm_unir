c9fafc5c93244f13b01d09dabfac421814237b03
jmtb <javier.munnozt@gmail.com>
2018-07-08 19:24:11 +0200

    Primera versión de auth controller

---
+++ unir-backend/swagger_server/controllers/token.py
@@ -0,0 +1,27 @@
+import jwt
+import configparser
+import swagger_server.controllers.bbdd as bd
+
+from swagger_server.models.credentials import Credentials  # noqa: E501
+from json import loads
+
+def check_crentials_token(token):
+
+    secret_key = get_token_secret()
+    
+    try:
+        decoded_token = jwt.decode(token, secret_key, algorithms=['HS256'])
+    except jwt.exceptions.InvalidSignatureError:
+        return None
+
+    return decoded_token["user_id"]
+    
+
+
+def get_token_secret():
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    secret_key = config['AUTH']['TOKEN_KEY']
+
+    return secret_key
