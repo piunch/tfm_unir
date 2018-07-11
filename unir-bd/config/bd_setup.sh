0adcc81ab69a2a70d03daefcbf660eb7682c52a4
jmtb <javier.munnozt@gmail.com>
2018-07-11 10:25:35 +0200

    ahora la BD coge por argumentos el usuario y la pass

--- unir-bd/config/bd_setup.sh
+++ unir-bd/config/bd_setup.sh
@@ -7,7 +7,7 @@ mysql < /usr/src/sql/bd_setup.sql
 
 echo "Creando usuario para la BD..."
 mysql -e "CREATE USER '${MYSQL_USER}' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON TFMUNIRBD.* TO 'tfmunir'@'%'; FLUSH PRIVILEGES;"
+mysql -e "GRANT ALL PRIVILEGES ON TFMUNIRBD.* TO '${MYSQL_USER}'@'%'; FLUSH PRIVILEGES;"
 
 echo "[Terminado]"
 mysql
